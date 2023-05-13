16674022617f2c68108455a007613fdb730f431e
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-13 12:37:46 +0200

    Merging

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore, Middleware } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 
