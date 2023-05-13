4bf8b1b1c33d2e2edb7964c69ba586620272f390
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-13 09:17:42 +0200

    Added store to the project

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {Provide} from 'react-redux';
+import store from './store.js';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provide store={store}>
+      <App />
+    </Provide>
   </React.StrictMode>,
 )
